18f92b8774d89f18b376cce61f068de3cb35ac75
hari-hech-kay <48200258+hari-hech-kay@users.noreply.github.com>
2020-06-10 14:43:41 +0530

    Prototype 2 done

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    private float speed = 20;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (transform.position.x < -10) 
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float spawnPosX;
+    private float spawnPosZ = 25;
+    public GameObject[] animalPrefabs;
+    private int animalIndex;
+    private float spawnTime = 2;
+    private float spawnDelay = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        animalIndex = Random.Range(0, animalPrefabs.Length);
+        spawnPosX = Random.Range(-10, 11);
+        Vector3 spawnPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
