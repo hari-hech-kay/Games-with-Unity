907b5493fec81a0c8b89003367d1485b586e5bbd
hari-hech-kay <48200258+hari-hech-kay@users.noreply.github.com>
2020-06-10 14:36:52 +0530

    Prototype 1 done

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20;
+    private float turnSpeed = 45;
+    public float horizontalInput;
+    public float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        if (verticalInput != 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        }
+       
+    }
+}
